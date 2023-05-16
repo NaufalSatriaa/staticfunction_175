08301bc72dc4e366bce06b031f9b064fa1f4575c
NaufalSatriaa <satriana0017@gmail.com>
2023-05-16 09:14:21 +0700

    membuat class mahasiswa

--- konstruktor_175/konstruktor_175.cpp
+++ konstruktor_175/konstruktor_175.cpp
@@ -2,3 +2,13 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(int iNIm, string iNama);
+	void cetak();
+};
