b0db623e9f03737b20a27ea08dcd440725252971
NaufalSatriaa <satriana0017@gmail.com>
2023-05-16 09:19:00 +0700

    menjelaskan method yang ada di class mahasiswa

--- konstruktor_175/konstruktor_175.cpp
+++ konstruktor_175/konstruktor_175.cpp
@@ -9,6 +9,22 @@ private:
 public:
 	mahasiswa();
 	mahasiswa(int);
+	mahasiswa(string);
 	mahasiswa(int iNIm, string iNama);
 	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
